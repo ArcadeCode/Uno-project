5a7b2d87768168af12bccdd5b413e8eebb4f5830
ArcadeCode <84012528+ArcadeCode@users.noreply.github.com>
2023-01-03 12:12:00 +0100

    Update Turn.py

--- core/motors/Turn.py
+++ core/motors/Turn.py
@@ -4,21 +4,19 @@ class TurnSystem() :
     def __init__(self) :
         self.isTurn = None
         self.isSkipped = False
-        
-    def __len__(self):
-        ''' Me demande pas Esteban du futur jsp pourquoi j ai besoin d appeler len() comme ça
-            Ouais bah finalement juste essaye pas de cap sa marche sans
-        '''
-        return len(self.list)
+        self.isReverse = False
     
     def returnValueInIndex(self, players, nb=1, substractive=False) :
         ''' Permet l'ajout de passer au prochain joueur
         de manière sécuriser en passant du dernier de la
         liste au premier type 'roulette' '''
-        indexMax = len(players)-1 # Stocke l'index maximun qu'on peut call avant error d'indexage 
-        while nb+1 > indexMax : # Technique de la roulette
-            nb = nb - indexMax
-        return nb+1
+        if self.isReverse == False :
+            indexMax = len(players)-1 # Stocke l'index maximun qu'on peut call avant error d'indexage 
+            while nb+1 > indexMax : # Technique de la roulette
+                nb = nb - indexMax
+            return nb+1
+        else : # Une carte reverse a été jouer donc on jour dans le sens inverse
+            pass
     
     def setTurn(self, players) :
         ''' Passe au tour suivant '''
@@ -34,7 +32,10 @@ class TurnSystem() :
     def skipTurn(self) : # Skip le tour suivant :
         self.isSkipped = True
     def reverse(self) :
-        pass
+        if self.isReverse == False :
+            self.isReverse = True
+        else : # If == True
+            self.isReverse = False
     
 turnSystem = TurnSystem()
 for i in range(0, 10) :

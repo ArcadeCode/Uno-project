a59e9eefaf9f7032fbe2183dc8871468574d2402
ArcadeCode <84012528+ArcadeCode@users.noreply.github.com>
2023-01-03 12:01:30 +0100

    Upgrade of main.py

--- core/main.py
+++ core/main.py
@@ -1,7 +1,6 @@
 '''
 Project made by Dervaux Esteban (LA FRANCE)
 It's a uno in python 3.10.0 (compatible 3.7.0)
-
 necessary modules :
 - random
 '''
@@ -10,10 +9,12 @@ necessary modules :
 from motors.game import Game, Player
 from motors.decks import motherClassDeck
 
-## Consts
-class Const :
+## Create consts :
+class Const : # On les stock dans un objet
     def __init__(self) -> None:
         self.version = "V.0.1"
+        self.acceptAI = False
+        self.lanParty = True
 const = Const()
 
 ## Creates Player
@@ -31,7 +32,47 @@ test = motherClassDeck()
 
 
 ## Init salon
+if const.lanParty == True :
+   class network() :
+      def __init__(self) -> None:
+         pass
+      def post() :
+         pass
+      def get() :
+         pass
+      def broadcast() :
+         pass
+
+      def waitOK() :
+         print("Êtes-vous prêt ?")
+         input("press enter to continue")
+         self.post()
+
 
 ## Init a game
+# Créer les 108 cartes
+# Donner 7 cartes à chaque joueurs
+# Placer les cartes restante dans la pioche
+# Prendre une carte de la pioche est la posé sur la table
 
 ## Play game
+win  = False
+turn = 0
+totalTurn = 0 # Cette varible sera stocké dans motors\turn plus tard
+while win == False or turn > 1000 :
+   
+   turn += 1
+   totalTurn += 1
+
+   # Sécurités anti bouclage
+   if turn > 1000 :
+      if const.lanParty == False : # <-- Partie local
+         print("Plus de", totalTurn,  "tours jouer voulez vous abandonner ?")
+         if input("Write 'Confirm' to end the party\n") == "Confirm" :
+            break
+         else :
+            turn = 0
+      else : # <--- Partie LAN
+         print("La partie a durée trop longtemps, la partie est terminé")
+         # Si la partie dure trop longtemps (1K de tours) on la supprime
+         break
